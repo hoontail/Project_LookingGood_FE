95d00f9efff8ddefcdd40574b66185005162a5b4
hoontail <sper456@naver.com>
2022-04-09 02:44:36 +0900

    메인페이지, 기본 컴포넌트구성

---
+++ hoontail/src/shared/Header.js
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "styled-components";
+import LoginBar from "../components/LoginBar";
+import Grid from "../elements/Grid";
+function Header(props) {
+  return (
+    <>
+      <Nav>
+          <Grid is_flex padding ="0px 0px 0px 16px"> 로고</Grid>
+          <Btns>  <LoginBar></LoginBar></Btns>
+      
+      </Nav>
+    </>
+  );
+}
+
+export default Header;
+
+const Nav = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  margin: 0 0;
+  height: 100px;
+  background-color: #fafafa;
+ 
+`;
+
+const Btns = styled.div`
+  float : right;
+`;
+
+
