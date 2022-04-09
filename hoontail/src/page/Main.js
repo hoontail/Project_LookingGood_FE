75236bd25a5bad5c4620000f4467586efc1f72a9
hoontail <sper456@naver.com>
2022-04-09 20:19:42 +0900

    뷰 완성

--- hoontail/src/page/Main.js
+++ hoontail/src/page/Main.js
@@ -6,14 +6,26 @@ function Main(props) {
   return (
     <>
       <Container>
-        <Menu><P>TEAM</P></Menu>      
-        <Menu><P>ALGORITHM</P></Menu>
-        <Menu><P>SKY</P></Menu> 
-        <Menu><P>COMMIT</P></Menu>
-        <Menu><P>FOOD</P></Menu>
-        <Menu><P>DIARY</P></Menu>
+        <Menu>
+          <P>TEAM</P>
+        </Menu>
+        <Menu>
+          <P>ALGORITHM</P>
+        </Menu>
+        <Menu>
+          <P>SKY</P>
+        </Menu>
+        <Menu>
+          <P>COMMIT</P>
+        </Menu>
+        <Menu>
+          <P>FOOD</P>
+        </Menu>
+        <Menu>
+          <P>DIARY</P>
+        </Menu>
       </Container>
-      <Footer/>
+      <Footer />
     </>
   );
 }
@@ -33,48 +45,42 @@ const Container = styled.div`
   /* border: 3px solid black; */
   justify-content: center;
   background-color: #fafafa;
-
 `;
 
-
 const Menu = styled.div`
-/* max-width: 430px; */
- width: 500px; 
-//   height: 200px;
-   margin: 0px 60px;
-//   /* background-color: blue; */
- display: flex;
- flex-direction: column;
-//   justify-content: center;
-   align-items: center;
- `;
- 
+  /* max-width: 430px; */
+  width: 500px;
+  //   height: 200px;
+  margin: 0px 60px;
+  //   /* background-color: blue; */
+  display: flex;
+  flex-direction: column;
+  //   justify-content: center;
+  align-items: center;
+`;
+
 const P = styled.p`
-  font-size: 110px;
+  font-size: 100px;
   margin-bottom: 15px;
   color: white;
   text-shadow: -1px 0px black, 0px 1px black, 1px 0px black, 0px -1px black;
-  cursor:pointer;
+  cursor: pointer;
   display: inline-block;
-   
-    -webkit-transition: 0.5s;
-    -moz-transition: 0.5s;
-    -o-transition: 0.5s;
-    -ms-transition: 0.5s;
-    transition: 0.5s;
-    &:hover{
-        color: rgb(0, 0, 0);
-        -webkit-transform: scale(1.2,1.2);
-    -moz-transform: scale(1.2,1.2);
-    -o-transform: scale(1.2,1.2);
-    -ms-transform: scale(1.2,1.2);
-    transform: scale(1.2,1.2);
 
-    }
- 
+  -webkit-transition: 0.5s;
+  -moz-transition: 0.5s;
+  -o-transition: 0.5s;
+  -ms-transition: 0.5s;
+  transition: 0.5s;
+  &:hover {
+    color: rgb(0, 0, 0);
+    -webkit-transform: scale(1.5, 1.5);
+    -moz-transform: scale(1.5, 1.5);
+    -o-transform: scale(1.5, 1.5);
+    -ms-transform: scale(1.5, 1.5);
+    transform: scale(1.5, 1.5);
+  }
 `;
-
-
 
 // a{
 //     display: inline-block;
